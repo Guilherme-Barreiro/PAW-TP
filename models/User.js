f291ad1e0302085eaf8adfc07eb356a51c2acd97
Guilherme Barreiro <80386801+Guilherme-Barreiro@users.noreply.github.com>
2025-04-08 21:57:21 +0100

    add cena de login

--- models/User.js
+++ models/User.js
@@ -3,8 +3,24 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Idealmente, encriptada
-  role: { type: String, enum: ['cliente', 'restaurante', 'admin'], required: true }
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['cliente', 'restaurante', 'admin'],
+    required: true
+  },
+  email: { type: String },
+  nomeCompleto: { type: String },
+  morada: { type: String },
+  telefone: { type: String },
+  dataNascimento: { type: Date },
+
+  // Só para clientes
+  historicoEncomendas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+
+  // Só para restaurantes
+  validado: { type: Boolean, default: false },
+  menus: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Menu' }]
 });
 
 module.exports = mongoose.model('User', userSchema);
