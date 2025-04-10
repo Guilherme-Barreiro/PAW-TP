8fa5ef93890080e18091b0ac4df4a1c95d0a2c9e
Guilherme Barreiro <80386801+Guilherme-Barreiro@users.noreply.github.com>
2025-04-10 20:24:58 +0100

    users - ler abaixo
    lista users
    remove users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET - Página de gestão de utilizadores
+router.get('/manage', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.render('userManage', { users });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao carregar utilizadores');
+  }
+});
+
+// POST - Eliminar utilizador
+router.post('/:id/delete', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.redirect('/users/manage');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao apagar utilizador');
+  }
 });
 
 module.exports = router;
